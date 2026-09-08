ba16942e98930dac2cec0f62de97ed95776736df
Santiago Zarate <santiago@zarate.co>
2026-09-08 09:00:51 +0200

    MockServer tests are now passing

--- t/client.t
+++ t/client.t
@@ -1,6 +1,4 @@
 use Mojo::Base -strict;
-## So we find the right libraries
-
 use FindBin;
 use lib "$FindBin::Bin/../lib";
 use lib "$FindBin::Bin/lib";
@@ -12,7 +10,7 @@ use Test::Cdmon::Mock::API;
 
 my $app = Test::Cdmon::Mock::API->new;
 
-subtest 'can_list_domains' => sub {
+subtest 'mock_server_can_list_domains' => sub {
 
     # my $t    = Test::Mojo->new('CdmonApi');
     my $t = Test::Mojo->new();
@@ -21,13 +19,14 @@ subtest 'can_list_domains' => sub {
     my $data = '{"data": { "domain": "test.com" }}';
 
     $t->post_ok( '/getDnsRecords', json => decode_json($data) )
-      ->status_is( 200, "Can talk to server" )
-      ->json_has( '/data/domain', 'Domain is present in response' )->or(
+      ->status_is( 200, "Can talk to server" )->json_like(
+        '/data/msg' => qr/test.com/,
+        'Domain is present in response'
+    )->or(
         sub {
             diag explain $t->tx->res;
         }
-      );
-    diag explain "no?";
+    );
 };
 
 done_testing;
--- t/lib/Test/Cdmon/Mock/API.pm
+++ t/lib/Test/Cdmon/Mock/API.pm
@@ -1,14 +1,45 @@
 package Test::Cdmon::Mock::API;
 use Mojolicious::Lite -signatures;
-use Mojo::JSON qw(decode_json encode_json);
+use Mojo::JSON   qw(decode_json encode_json);
+use Mojo::Loader qw(data_section);
+
+my $dns_records =
+  decode_json( data_section __PACKAGE__, 'getDndRecords.json.ep' );
 
 post '/getDnsRecords' => sub ($c) {
     my $data = $c->req->json;
-    $c->log->trace( "getDnsRecords:" . encode_json($data) );
+    $c->log->trace( "getDnsRecords data" . encode_json($data) );
     say "getDnsRecords: ", encode_json($data);
-    $c->render( json => $data );
+
+    # app->log->trace("foo - $_") for keys %{ data_section __PACKAGE__ };
+    # app->log->debug($_)         for keys %{ data_section __PACKAGE__ };
+    $c->render( json => $dns_records );
 };
 
 app->start;
 
 1;
+
+__DATA__
+
+@@ getDndRecords.json.ep
+{
+    "status" : "ok",
+      "data" : {
+        "msg" : "DNS records for domain test.com",
+        "result" : [
+            {
+                "type" : "TXT",
+                "host" : "_dmarc",
+                "ttl" : 900,
+                "value" : "v=DMARC1; p=none; aspf=s; adkim=r;"
+            },
+            {
+                "type" : "NS",
+                "host" : "@",
+                "ttl" : 3600,
+                "value" : "ns1.cdmon.net"
+            }
+        ]
+      }
+}
